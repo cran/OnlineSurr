8f95219664482f3847d90040ad2f589d226510b3
Silvaneo dos Santos Jr. <silvaneojunior@utexas.edu>
2026-04-22 20:30:16 +0000

    version 0.0.4

--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -6,57 +6,16 @@ knitr::opts_chunk$set(
   warning = FALSE
 )
 
-## -----------------------------------------------------------------------------
-set.seed(1)
-
-# Dimensions
-N <- 100 # subjects
-T <- 6 # time points
-times <- seq_len(T) # numeric, equally spaced
-
-# Subject IDs and treatment assignment (two levels)
-id <- rep(seq_len(N), each = T)
-trt <- rep(rbinom(N, 1, 0.5), each = T) # 0/1
-
-time <- rep(times, times = N)
-
-# Simulate surrogate(s) and outcome
-# Surrogate is affected by treatment and time
-s <-
-  0.2 * time + # Trend
-  (0.2 + 0.4 * time) * trt + # Treatment effect
-  rnorm(N * T, sd = 0.1) # Noise
-
-
-# Outcome depends on treatment effect, time trend, surrogate, and noise
-y <-
-  0.2 + # intercept
-  0.1 * time + # Trend
-  (0.2 + 0.1 * time) * trt + # (direct) treatment effect
-  0.6 * s + # Surrogate effect
-  rnorm(N * T, sd = 0.1) # Noise
-
-dat <- data.frame(
-  id = id,
-  trt = trt,
-  time = time,
-  s = s,
-  y = y
-)
-
-# Ensure the data are ordered by (id, time) (recommended)
-dat <- dat[order(dat$id, dat$time), ]
-head(dat)
-
 ## ----eval=TRUE----------------------------------------------------------------
 library(OnlineSurr)
+head(sim_onlinesurr)
 
 fit <- fit.surr(
   formula   = y ~ 1, # baseline fixed effects; trt*time terms added internally
   id        = id,
   surrogate = ~s, # surrogate structure
   treat     = trt,
-  data      = dat,
+  data      = sim_onlinesurr,
   time      = time,
   N.boots   = 2000, # bootstrap draws stored in the fitted object
   verbose   = 0 # hide progress
@@ -70,7 +29,7 @@ fit <- fit.surr(
 #   id        = id,
 #   surrogate = ~ s(s) + s(lagged(s, 1)) + s(lagged(s, 2)), # surrogate structure
 #   treat     = trt,
-#   data      = dat,
+#   data      = sim_onlinesurr,
 #   time      = time,
 #   verbose   = 0 # hide progress
 # )
